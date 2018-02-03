bd550d69fc1be2699b7a58079cbcfe58eb598d40
NeroM1 <36106056+NeroM1@users.noreply.github.com>
2018-02-03 15:19:27 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -47,6 +47,14 @@ while (x <= readcount) {
 /// End data updates
 GPS_Data.close();
 
+/// a function that calculates distance between two points on a sphere
+double distance(double lat1, double lon1, double lat2, double lon2) {
+    /// constants for calculations
+    const int earth_radius = 6371000;
+    double d = 2 * earth_radius * asin(sqrt(pow(sin((lat2-lat1)/2),2)+cos(lat1)*cos(lat2)*pow(sin((lon2-lon1)/2),2)));
+    return d;
+}
+
 
 /// IMU Data Acquisition
 /// Ethan 
