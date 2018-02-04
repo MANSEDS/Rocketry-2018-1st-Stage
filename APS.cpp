afcd7d661921e301b39daef403b2d5b3b6791e6a
NeroM1 <36106056+NeroM1@users.noreply.github.com>
2018-02-04 11:59:38 +0000

    Update APS.cpp

--- APS.cpp
+++ APS.cpp
@@ -28,20 +28,11 @@ void APS::GPS_data_in(std::string GPS_data) {
         }
 
         else if (x == 4) {
-
             if (_string1 == "N") {
-                _num = 0;
+                _GPS_lon = stod(_string2);
             }
             else if (_string1 == "S") {
-                _num = 1;
-            }
-            switch (_num) {
-                case 0:
-                    _GPS_lon = stod(_string2);
-                case 1:
-                    _GPS_lon = -1 * stod(_string2);
-                default:
-                    _GPS_lon = 0;
+                _GPS_lon = -1 * stod(_string2);
             }
         }
 
@@ -51,19 +42,14 @@ void APS::GPS_data_in(std::string GPS_data) {
 
         else if (x == 6) {
             if (_string1 == "E") {
-                _num = 0;
+                _GPS_lat = stod(_string2);
             }
             else if (_string1 == "W") {
-                _num = 1;
+                _GPS_lat = -1 * stod(_string2);
             }
-            switch (_num) {
-                case 0:
-                    _GPS_lat = stod(_string2);
-                case 1:
-                    _GPS_lat = -1 * stod(_string2);
-                default:
-                    _GPS_lat = 0;
-            }
+        }
+        else if (x == 10) {
+            _GPS_alt = stod(_string1);
         }
         x++;
     }
@@ -72,6 +58,43 @@ void APS::GPS_data_in(std::string GPS_data) {
 
 std::string APS::GLO_data_in(std::string GLO_data) {
     _GLO_data = GLO_data;
+    int x = 0;
+    std::stringstream ss(GLO_data);
+    while(getline(ss,_string1,_spacing)) {
+        if (x == 1) {
+            _GLO_time = stod(_string1);
+        }
+
+        else if (x == 3) {
+            _string2 = _string1;
+        }
+
+        else if (x == 4) {
+            if (_string1 == "N") {
+                _GLO_lon = stod(_string2);
+            }
+            else if (_string1 == "S") {
+                _GLO_lon = -1 * stod(_string2);
+            }
+        }
+
+        else if (x == 5) {
+            _string2 = _string1;
+        }
+
+        else if (x == 6) {
+            if (_string1 == "E") {
+                _GLO_lat = stod(_string2);
+            }
+            else if (_string1 == "W") {
+                _GLO_lat = -1 * stod(_string2);
+            }
+        }
+        else if (x == 10) {
+            _GLO_alt = stod(_string1);
+        }
+        x++;
+    }
 }
 
 /// Get GPS data
@@ -91,18 +114,26 @@ double APS::get_GPS_lat() {
     return _GPS_lat;
 }
 
+double APS::get_GPS_alt() {
+    return _GPS_alt;
+}
+
 /// Get GLONASS data
 std::string APS::get_GLO_data() {
-    return _GPS_data;
+    return _GLO_data;
 }
 double APS::get_GLO_time() {
-    return _GPS_time;
+    return _GLO_time;
 }
 
 double APS::get_GLO_lon() {
-    return _GPS_lon;
+    return _GLO_lon;
 }
 
 double APS::get_GLO_lat() {
-    return _GPS_lat;
+    return _GLO_lat;
+}
+
+double APS::get_GLO_alt() {
+    return _GLO_alt;
 }
