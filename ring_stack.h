9f37f79e28284e4c3d312004332711c492d00417
Alex Koch <30006902+Alex-Koch-2200@users.noreply.github.com>
2018-02-25 14:17:23 +0000

    Update ring_stack.h
    The previous version simply did not work. This implementation should work for our purposes. Be careful when using this stack as it has no function for checking if it is full or not.

--- ring_stack.h
+++ ring_stack.h
@@ -13,39 +13,39 @@
     a number of times equal to the 'size_' of the stack.
 */
 
-template<typename T> class ring_stack
+// A horribly unsafe yet very fast implementation of a stack for use in the rocket code. Please understand it's limitations before using this class
+template<typename T> class f_ring_stack
 {
 private:
-    T * container_;
-    uint32_t top_, shift_ increment_;
+	T * container_;
+	uint32_t top_, shift_, increment_;
 public:
-    ring_stack(unsigned bits) : top_{ 0 }, shift_{ uint32_t(32 - bits) }, 
-                                increment_{ uint32_t( pow(2, 32) / pow(2, bits) ) }, 
-                                container_{ new T[ uint32_t( pow(2, bits) ) ] } 
-                                { 
-                                    container_[0] = nullptr; // set the first element to nullptr so that it can be checked if the array has been filled
-                                }
-    bool empty() { return container_[0] == nullptr && top_ == 0; }
-    bool full() { return container_[0] != nullptr; } // Container is filled only once the 0th element has been overwritten via a push() call
-    uint16_t size() 
-    { 
-        if(container_[0] == nullptr)
-        {
-            return top_ >> shift_; // The shifted value of top_ corresponds to the number of elements in the stack
-        }
-        else{ return max_size; } // The stack has been filled and so the number of elements is always the max size as any new ones will overwrite the oldest element.
-    }
-    uint32_t max_size() { return pow(32 - shift_, 2); }
-    T & top() { return container_[top_ >> increment_]; }
-    void push(const T element) 
-    {   
-        top_ += increment_; // Must shift the top first so that it corresponds to the slot in which to place the new element
-        container_[top_ >> shift_] = element;
-    }
-    // emplace?
-    void pop() 
-    {
-        top_ -= increment_; // Deincrement as this is a LIFO queue
-    }
-    // swap?
-}
+	f_ring_stack() : top_{ 0 }, container_ { nullptr }, top_{ 0 }, shift_{ 0 }, increment_{ 0 } {}
+	f_ring_stack(unsigned bits) : shift_{ uint32_t(32 - bits) }, increment_{ uint32_t(pow(2, 32) / pow(2, bits)) }, container_{ new T[uint32_t(pow(2, bits))] } {}
+	~f_ring_stack() 
+	{
+		delete[] container_;
+	}
+	uint32_t max_size() { return pow(32 - shift_, 2); }
+	T & top() { return container_[top_ >> shift_]; }
+	void push(const T element)
+	{
+		top_ += increment_; // Must shift the top first so that it corresponds to the slot in which to place the new element
+		container_[top_ >> shift_] = element;
+	}
+	// emplace?
+	void pop()
+	{
+		container_[top_ >> shift_].~T(); // Call the objects destructor
+		top_ -= increment_; // Deincrement as this is a LIFO queue
+	}
+	// Define an overload for the accessor operator []
+	T & operator [] (uint32_t position)
+	{
+		std::cout << "pos before = " << position << std::endl;
+		position << shift_;
+		position = top_ - position;
+		std::cout << "pos after = " << position << std::endl;
+		return container_[position >> shift_];
+	}
+};
